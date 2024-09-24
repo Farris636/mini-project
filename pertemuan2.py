508815b58745d16b2873aebe2d441c1ad3b9cae6
Farris636 <153788374+Farris636@users.noreply.github.com>
2024-09-24 14:11:41 +0800

    Add files via upload

---
+++ pertemuan2.py
@@ -0,0 +1,14 @@
+umur = int(input("masukkan umur anda"))
+
+if umur >= 30:
+    if umur >= 30:
+        print("selamat pagi pak")
+elif umur >= 20:
+    if umur >= 20:
+        print("selamat pagi mas")
+elif umur >= 18:
+    print("selamat pagi bro")
+elif umur >= 10:
+    print("selamat pagi dek")
+else:
+    print("anda dibawah umur")
